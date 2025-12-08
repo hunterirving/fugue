bc9003926b825bd6712cc5883292f7a6f2fe6262
Hunter <hunter@hunterirving.com>
2025-12-08 17:24:48 -0500

    first pass at PWA support

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'fugue-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/resources/fonts/Special_Elite/SpecialElite-Regular.ttf',
+  '/resources/icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys.filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((cached) => cached || fetch(event.request))
+  );
+});
